06cd5ad99db738a36c29bc41dfcaaea9e972d534
Neil45623 <86229955+Neil45623@users.noreply.github.com>
2022-10-13 09:44:17 +0200

    modif chien

--- src/components/Chien.jsx
+++ src/components/Chien.jsx
@@ -1,26 +1,27 @@
-import React from "react";
+import React, {useState} from 'react';
 import Navbar from './Navbar';
-import style from "./Chien.module.css";
-import getRandomBreed from "./Api";
-
+import style from './Chien.module.css'
 
 function Chien() {
-    return (
-        <><div className={style.Navbar2}><Navbar></Navbar></div><form>
-            <title className={style.titre}>Le chien</title>
-            <label className={style.race}>Race du chien
-            </label>
-            <p className={style.Age}>Age du chien
-            </p>
-            <p className={style.Prix}>Prix du chien
-            </p>
-        </form></>
-    )
+
+  const [dogPic, setDogPic] = useState("");
+
+  async function getPic(){
+    const response = await fetch('https://dog.ceo/api/breeds/image/random');
+    const data = await response.json();
+    console.log(data.message);
+    setDogPic(data.message); 
   }
 
+  return (
+    <><div className={style.Navbar2}><Navbar></Navbar></div><>
 
-
+          <div className={style.img2}>
+          <button className={style.button} onClick={getPic}>Next image</button>
+              <img src={dogPic} alt='Very important picture' />
+          </div>
+      </></>
+  );
+}
 
 export default Chien;
-
-   
